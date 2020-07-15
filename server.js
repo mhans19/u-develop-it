7db2f7e8e00dd6b5702618656ea8aa721c36f7b7
mhans19 <mdhansen19@gmail.com>
2020-07-14 23:07:08 -0600

    set up port and verify express

---
+++ server.js
@@ -0,0 +1,20 @@
+// Dependencies
+const express = require('express');
+// Port
+const PORT = process.env.PORT || 3001;
+const app = express();
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+
+
+// Default response for any other request(Not Found) Catch all
+app.use((req, res) => {
+    res.status(404).end();
+  });
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
