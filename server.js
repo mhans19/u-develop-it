e14a1ebb77e371a633670c98a454bbdc68997f11
mhans19 <mdhansen19@gmail.com>
2020-07-15 18:51:22 -0600

    added left join on tables

--- server.js
+++ server.js
@@ -18,15 +18,18 @@ const db = new sqlite3.Database('./db/election.db', err => {
 
 // Get single candidate
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates 
-                 WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id 
+             WHERE candidates.id = ?`;
     const params = [req.params.id];
     db.get(sql, params, (err, row) => {
       if (err) {
         res.status(400).json({ error: err.message });
         return;
       }
-  
       res.json({
         message: 'success',
         data: row
@@ -74,7 +77,11 @@ app.post('/api/candidate', ({ body }, res) => {
 });
 // Get all candidates
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id`;
     const params = [];
     db.all(sql, params, (err, rows) => {
       if (err) {
